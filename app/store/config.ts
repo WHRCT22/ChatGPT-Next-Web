a9126d34e6496c8927bd3c086f058ad4af115336
WHRCT22 <125719278+WHRCT22@users.noreply.github.com>
2023-08-01 17:06:04 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -86,7 +86,7 @@ export const ALL_MODELS = [
   },
   {
     name: "gpt-3.5-turbo-0301",
-    available: true,
+    available: false,
   },
   {
     name: "gpt-3.5-turbo-0613",
@@ -94,11 +94,11 @@ export const ALL_MODELS = [
   },
   {
     name: "gpt-3.5-turbo-16k",
-    available: true,
+    available: false,
   },
   {
     name: "gpt-3.5-turbo-16k-0613",
-    available: true,
+    available: false,
   },
   {
     name: "qwen-v1", // 通义千问
