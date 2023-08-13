07133505d8280d7282fad7b7cbc2ac998568408f
WHRCT22 <125719278+WHRCT22@users.noreply.github.com>
2023-08-13 11:09:16 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -31,14 +31,14 @@ export const DEFAULT_CONFIG = {
   dontShowMaskSplashScreen: false, // dont show splash screen when create chat
 
   modelConfig: {
-    model: "gpt-3.5-turbo" as ModelType,
+    model: "gpt-4" as ModelType,
     temperature: 0.5,
     top_p: 1,
     max_tokens: 4000,
     presence_penalty: 0,
     frequency_penalty: 0,
     sendMemory: true,
-    historyMessageCount: 4,
+    historyMessageCount: 10,
     compressMessageLengthThreshold: 1000,
     template: DEFAULT_INPUT_TEMPLATE,
   },
@@ -53,7 +53,7 @@ export type ChatConfigStore = ChatConfig & {
 
 export type ModelConfig = ChatConfig["modelConfig"];
 
-const ENABLE_GPT4 = false;
+const ENABLE_GPT4 = true;
 
 export const ALL_MODELS = [
   {
