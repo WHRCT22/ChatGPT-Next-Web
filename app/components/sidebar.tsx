f2a7cfeb35009fd9745272dbcd55fc423afa703f
Symobi <103533638+Symobi@users.noreply.github.com>
2023-08-13 22:16:00 -0400

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,10 +117,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT Web X
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          The greatest ChatGPT.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
