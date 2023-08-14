d919327afda1832c14d71e5f824b214b1043586d
Symobi <103533638+Symobi@users.noreply.github.com>
2023-08-13 22:17:44 -0400

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -120,7 +120,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT Web X
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          The greatest ChatGPT.
+          The greatest ChatGPT web app.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
