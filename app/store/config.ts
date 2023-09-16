e65aa2d6e2c79a5c35b1c5bed07fa15d94e22b20
WHRCT22 <125719278+WHRCT22@users.noreply.github.com>
2023-09-16 10:56:39 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -20,7 +20,7 @@ export enum Theme {
 export const DEFAULT_CONFIG = {
   submitKey: SubmitKey.Enter as SubmitKey,
   avatar: "1f603",
-  fontSize: 16,
+  fontSize: 18,
   theme: Theme.Auto as Theme,
   tightBorder: !!getClientConfig()?.isApp,
   sendPreviewBubble: true,
