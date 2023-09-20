38abb4f4ad5f28ba32100f83a38694bcdce9b980
WHRSTUDIO <125719278+WHRCT22@users.noreply.github.com>
2023-09-20 08:49:06 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,10 +117,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT Web X 2.0
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Powered By WHRSTUDIO
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
